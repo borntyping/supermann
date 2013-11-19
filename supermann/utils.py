c01f501a09969cb45200149d79af775a3176d610
Sam Clements <sam.clements@datasift.com>
2013-11-19 10:24:03 +0000

    Minor logging changes

--- supermann/utils.py
+++ supermann/utils.py
@@ -4,9 +4,18 @@ from __future__ import absolute_import, unicode_literals
 
 import logging
 
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+logging.getLogger('supermann').addHandler(NullHandler())
+
+
 def fullname(obj):
     """Returns the qualified name of an object's class"""
     return '.'.join([obj.__module__, obj.__class__.__name__])
+
 
 def getLogger(obj):
     """Returns a logger using the full name of the given object"""
