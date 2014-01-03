borntyping/supermann

commit 48237f89c39bc82e2090a35604ba802439e37869

Replaced supermann.riemann with riemann-client
https://github.com/borntyping/python-riemann-client

setup.py
@@ -7,7 +7,7 @@ import sys
 
 setuptools.setup(
     name = "supermann",
-    version = '1.1.3',
+    version = '1.2.0',
 
     author = "Sam Clements",
     author_email = "sam.clements@datasift.com",
@@ -23,6 +23,7 @@ setuptools.setup(
         'blinker==1.3',
         'protobuf==2.5.0',
         'psutil==1.2.0',
+        'riemann-client==2.0.1',
         'supervisor==3.0',
     ],
 

supermann/__init__.py
@@ -4,6 +4,6 @@ from __future__ import absolute_import, unicode_literals
 
 from supermann.core import Supermann
 
-__version__ = '1.1.3'
+__version__ = '1.2.0'
 __author__ = 'Sam Clements <sam.clements@datasift.com>'
 __all__ = ['Supermann']

supermann/core.py
@@ -4,12 +4,11 @@ from __future__ import absolute_import, unicode_literals
 
 import collections
 import os
-import traceback
 
 import psutil
+import riemann
 
 import supermann.metrics
-import supermann.riemann.client
 import supermann.signals
 import supermann.supervisor
 
@@ -17,17 +16,16 @@ import supermann.supervisor
 class Supermann(object):
     """The main Supermann process"""
 
-    def __init__(self, host='localhost', port=5555):
+    def __init__(self, host=None, port=None):
         self.log = supermann.utils.getLogger(self)
         self.log.info("This looks like a job for Supermann!")
-
         self.actions = collections.defaultdict(list)
 
-        # The Supervisor interface reads configuration from the environment
+        # The Supervisor and Riemann clients read their configuration from
+        # the environment, though the Riemann client can be overridden
         self.supervisor = supermann.supervisor.Supervisor()
-
-        # But Riemann does need configuring
-        self.riemann = supermann.riemann.client.UDPClient(host, port)
+        self.riemann_transport = riemann.client.TCPTransport(host, port)
+        self.riemann = riemann.client.QueuedClient(self.riemann_transport)
 
     def connect(self, signal, reciver):
         """Connects a signal that will recive messages from this instance"""
@@ -43,7 +41,7 @@ class Supermann(object):
                 # Emit a signal for each Supervisor subprocess
                 self.emit_processes(event=event)
                 # Send the queued events at the end of the cycle
-                self.riemann.send_next_message()
+                self.riemann.flush()
         except Exception as exception:
             self.log.exception("A fatal exception has occurred:")
             raise exception

supermann/riemann/__init__.py
@@ -1,1 +1,0 @@
-"""Riemann client for Supermann"""

supermann/command.py
@@ -36,7 +36,7 @@ def formatter_class(*args, **kwargs):
     """Builds a modified argparse formatter"""
     kwargs.setdefault('max_help_position', 32)
     kwargs.setdefault('width', 96)
-    return argparse.ArgumentDefaultsHelpFormatter(*args, **kwargs)
+    return argparse.HelpFormatter(*args, **kwargs)
 
 
 parser = argparse.ArgumentParser(formatter_class=formatter_class)
@@ -51,10 +51,10 @@ parser.add_argument(
     default='INFO', choices=LOG_LEVELS.keys(),
     help="One of CRITICAL, ERROR, WARNING, INFO, DEBUG")
 parser.add_argument(
-    'host', type=str, nargs='?', default='localhost',
+    'host', type=str, nargs='?', default=None,
     help="The Riemann server to connect to")
 parser.add_argument(
-    'port', type=int, nargs='?', default=5555,
+    'port', type=int, nargs='?', default=None,
     help="The Riemann server to connect to")
 
 
@@ -68,16 +68,16 @@ def main():
     self = supermann.core.Supermann(host=args.host, port=args.port)
     self.check_supervisor()
 
-    # A tick signal should be emitted when Supervisord ticks
+    # Collect system metrics when an event is received
     self.connect(supermann.signals.event, supermann.metrics.system.cpu)
     self.connect(supermann.signals.event, supermann.metrics.system.mem)
     self.connect(supermann.signals.event, supermann.metrics.system.swap)
 
-    # Supermann should emit a signal for each process being monitored
+    # Collect metrics for each process when an event is recived
     self.connect(supermann.signals.process, supermann.metrics.process.cpu)
     self.connect(supermann.signals.process, supermann.metrics.process.mem)
     self.connect(supermann.signals.process, supermann.metrics.process.fds)
     self.connect(supermann.signals.process, supermann.metrics.process.state)
 
-    # Supermann will then attempt to run forever
+    # Supermann will attempt to run forever
     self.run()

supermann/riemann/client.py
@@ -1,79 +1,0 @@
-"""Riemann protocol buffer client"""
-
-from __future__ import absolute_import, unicode_literals
-
-import abc
-import socket
-
-import supermann.riemann.riemann_pb2
-import supermann.utils
-
-
-class Client(object):
-    """An abstract Riemann client"""
-
-    __metaclass__ = abc.ABCMeta
-
-    def __init__(self, host, port=5555):
-        self.log = supermann.utils.getLogger(self)
-        self.log.info("Sending messages to Riemann at %s:%s", host, port)
-        self.host = host
-        self.port = port
-        self.create_next_message()
-
-    def __enter__(self):
-        self.connect()
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.disconnect()
-
-    @abc.abstractmethod
-    def connect(self):
-        pass
-
-    @abc.abstractmethod
-    def disconnect(self):
-        pass
-
-    @abc.abstractmethod
-    def write(self):
-        pass
-
-    def create_next_message(self):
-        """Creates a Msg object to act as a queue"""
-        self.next = supermann.riemann.riemann_pb2.Msg()
-
-    def queue_events(self, *events):
-        """Adds events to the next message"""
-        self.next.events.extend(events)
-
-    def send_next_message(self):
-        """Sends the waiting message to Riemann"""
-        self.log.debug("Sending message with {n} events to Riemann".format(
-            n=len(self.next.events)))
-        self.write(self.next)
-        self.create_next_message()
-
-    def event(self, service, **data):
-        """Creates an event and adds it to the next message"""
-        event = supermann.riemann.riemann_pb2.Event()
-        event.service = service
-        event.tags.append('supermann')
-        event.tags.extend(data.pop('tags', list()))
-        data.setdefault('host', socket.gethostname())
-        for name, value in data.items():
-            setattr(event, name, value)
-        self.queue_events(event)
-
-
-class UDPClient(Client):
-    def connect(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    def disconnect(self):
-        self.socket.close()
-
-    def write(self, message):
-        self.socket.sendto(message.SerializeToString(), (self.host, self.port))
-        return message

supermann/riemann/riemann.proto
@@ -1,45 +1,0 @@
-option java_package = "com.aphyr.riemann";
-option java_outer_classname = "Proto";
-
-message State {
-  optional int64 time = 1;
-  optional string state = 2;
-  optional string service = 3;
-  optional string host = 4;
-  optional string description = 5;
-  optional bool once = 6;
-  repeated string tags = 7;
-  optional float ttl = 8;
-}
-
-message Event {
-  optional int64 time = 1;
-  optional string state = 2;
-  optional string service = 3;
-  optional string host = 4;
-  optional string description = 5;
-  repeated string tags = 7;
-  optional float ttl = 8;
-  repeated Attribute attributes = 9;
-
-  optional sint64 metric_sint64 = 13;
-  optional double metric_d = 14;
-  optional float metric_f = 15;
-}
-
-message Query {
-  optional string string = 1;
-}
-
-message Msg {
-  optional bool ok = 2;
-  optional string error = 3;
-  repeated State states = 4;
-  optional Query query = 5;
-  repeated Event events = 6;
-}
-
-message Attribute {
-  required string key = 1;
-  optional string value = 2;
-}

supermann/riemann/riemann_pb2.py
@@ -1,352 +1,0 @@
-# Generated by the protocol buffer compiler.  DO NOT EDIT!
-
-from google.protobuf import descriptor
-from google.protobuf import message
-from google.protobuf import reflection
-from google.protobuf import descriptor_pb2
-# @@protoc_insertion_point(imports)
-
-
-
-DESCRIPTOR = descriptor.FileDescriptor(
-  name='riemann.proto',
-  package='',
-  serialized_pb='\n\rriemann.proto\"\x81\x01\n\x05State\x12\x0c\n\x04time\x18\x01 \x01(\x03\x12\r\n\x05state\x18\x02 \x01(\t\x12\x0f\n\x07service\x18\x03 \x01(\t\x12\x0c\n\x04host\x18\x04 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x05 \x01(\t\x12\x0c\n\x04once\x18\x06 \x01(\x08\x12\x0c\n\x04tags\x18\x07 \x03(\t\x12\x0b\n\x03ttl\x18\x08 \x01(\x02\"\xce\x01\n\x05\x45vent\x12\x0c\n\x04time\x18\x01 \x01(\x03\x12\r\n\x05state\x18\x02 \x01(\t\x12\x0f\n\x07service\x18\x03 \x01(\t\x12\x0c\n\x04host\x18\x04 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x05 \x01(\t\x12\x0c\n\x04tags\x18\x07 \x03(\t\x12\x0b\n\x03ttl\x18\x08 \x01(\x02\x12\x1e\n\nattributes\x18\t \x03(\x0b\x32\n.Attribute\x12\x15\n\rmetric_sint64\x18\r \x01(\x12\x12\x10\n\x08metric_d\x18\x0e \x01(\x01\x12\x10\n\x08metric_f\x18\x0f \x01(\x02\"\x17\n\x05Query\x12\x0e\n\x06string\x18\x01 \x01(\t\"g\n\x03Msg\x12\n\n\x02ok\x18\x02 \x01(\x08\x12\r\n\x05\x65rror\x18\x03 \x01(\t\x12\x16\n\x06states\x18\x04 \x03(\x0b\x32\x06.State\x12\x15\n\x05query\x18\x05 \x01(\x0b\x32\x06.Query\x12\x16\n\x06\x65vents\x18\x06 \x03(\x0b\x32\x06.Event\"\'\n\tAttribute\x12\x0b\n\x03key\x18\x01 \x02(\t\x12\r\n\x05value\x18\x02 \x01(\tB\x1a\n\x11\x63om.aphyr.riemannB\x05Proto')
-
-
-
-
-_STATE = descriptor.Descriptor(
-  name='State',
-  full_name='State',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  fields=[
-    descriptor.FieldDescriptor(
-      name='time', full_name='State.time', index=0,
-      number=1, type=3, cpp_type=2, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='state', full_name='State.state', index=1,
-      number=2, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='service', full_name='State.service', index=2,
-      number=3, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='host', full_name='State.host', index=3,
-      number=4, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='description', full_name='State.description', index=4,
-      number=5, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='once', full_name='State.once', index=5,
-      number=6, type=8, cpp_type=7, label=1,
-      has_default_value=False, default_value=False,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='tags', full_name='State.tags', index=6,
-      number=7, type=9, cpp_type=9, label=3,
-      has_default_value=False, default_value=[],
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='ttl', full_name='State.ttl', index=7,
-      number=8, type=2, cpp_type=6, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  options=None,
-  is_extendable=False,
-  extension_ranges=[],
-  serialized_start=18,
-  serialized_end=147,
-)
-
-
-_EVENT = descriptor.Descriptor(
-  name='Event',
-  full_name='Event',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  fields=[
-    descriptor.FieldDescriptor(
-      name='time', full_name='Event.time', index=0,
-      number=1, type=3, cpp_type=2, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='state', full_name='Event.state', index=1,
-      number=2, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='service', full_name='Event.service', index=2,
-      number=3, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='host', full_name='Event.host', index=3,
-      number=4, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='description', full_name='Event.description', index=4,
-      number=5, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='tags', full_name='Event.tags', index=5,
-      number=7, type=9, cpp_type=9, label=3,
-      has_default_value=False, default_value=[],
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='ttl', full_name='Event.ttl', index=6,
-      number=8, type=2, cpp_type=6, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='attributes', full_name='Event.attributes', index=7,
-      number=9, type=11, cpp_type=10, label=3,
-      has_default_value=False, default_value=[],
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='metric_sint64', full_name='Event.metric_sint64', index=8,
-      number=13, type=18, cpp_type=2, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='metric_d', full_name='Event.metric_d', index=9,
-      number=14, type=1, cpp_type=5, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='metric_f', full_name='Event.metric_f', index=10,
-      number=15, type=2, cpp_type=6, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  options=None,
-  is_extendable=False,
-  extension_ranges=[],
-  serialized_start=150,
-  serialized_end=356,
-)
-
-
-_QUERY = descriptor.Descriptor(
-  name='Query',
-  full_name='Query',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  fields=[
-    descriptor.FieldDescriptor(
-      name='string', full_name='Query.string', index=0,
-      number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  options=None,
-  is_extendable=False,
-  extension_ranges=[],
-  serialized_start=358,
-  serialized_end=381,
-)
-
-
-_MSG = descriptor.Descriptor(
-  name='Msg',
-  full_name='Msg',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  fields=[
-    descriptor.FieldDescriptor(
-      name='ok', full_name='Msg.ok', index=0,
-      number=2, type=8, cpp_type=7, label=1,
-      has_default_value=False, default_value=False,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='error', full_name='Msg.error', index=1,
-      number=3, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='states', full_name='Msg.states', index=2,
-      number=4, type=11, cpp_type=10, label=3,
-      has_default_value=False, default_value=[],
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='query', full_name='Msg.query', index=3,
-      number=5, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='events', full_name='Msg.events', index=4,
-      number=6, type=11, cpp_type=10, label=3,
-      has_default_value=False, default_value=[],
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  options=None,
-  is_extendable=False,
-  extension_ranges=[],
-  serialized_start=383,
-  serialized_end=486,
-)
-
-
-_ATTRIBUTE = descriptor.Descriptor(
-  name='Attribute',
-  full_name='Attribute',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  fields=[
-    descriptor.FieldDescriptor(
-      name='key', full_name='Attribute.key', index=0,
-      number=1, type=9, cpp_type=9, label=2,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='value', full_name='Attribute.value', index=1,
-      number=2, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  options=None,
-  is_extendable=False,
-  extension_ranges=[],
-  serialized_start=488,
-  serialized_end=527,
-)
-
-_EVENT.fields_by_name['attributes'].message_type = _ATTRIBUTE
-_MSG.fields_by_name['states'].message_type = _STATE
-_MSG.fields_by_name['query'].message_type = _QUERY
-_MSG.fields_by_name['events'].message_type = _EVENT
-DESCRIPTOR.message_types_by_name['State'] = _STATE
-DESCRIPTOR.message_types_by_name['Event'] = _EVENT
-DESCRIPTOR.message_types_by_name['Query'] = _QUERY
-DESCRIPTOR.message_types_by_name['Msg'] = _MSG
-DESCRIPTOR.message_types_by_name['Attribute'] = _ATTRIBUTE
-
-class State(message.Message):
-  __metaclass__ = reflection.GeneratedProtocolMessageType
-  DESCRIPTOR = _STATE
-  
-  # @@protoc_insertion_point(class_scope:State)
-
-class Event(message.Message):
-  __metaclass__ = reflection.GeneratedProtocolMessageType
-  DESCRIPTOR = _EVENT
-  
-  # @@protoc_insertion_point(class_scope:Event)
-
-class Query(message.Message):
-  __metaclass__ = reflection.GeneratedProtocolMessageType
-  DESCRIPTOR = _QUERY
-  
-  # @@protoc_insertion_point(class_scope:Query)
-
-class Msg(message.Message):
-  __metaclass__ = reflection.GeneratedProtocolMessageType
-  DESCRIPTOR = _MSG
-  
-  # @@protoc_insertion_point(class_scope:Msg)
-
-class Attribute(message.Message):
-  __metaclass__ = reflection.GeneratedProtocolMessageType
-  DESCRIPTOR = _ATTRIBUTE
-  
-  # @@protoc_insertion_point(class_scope:Attribute)
-
-# @@protoc_insertion_point(module_scope)

commit 58e3c923e91618517e1c39b0152cdeaa4b8093af

Use the riemann-client libary correctly
Fixes errors introduced in 48237f8

setup.py
@@ -7,7 +7,7 @@ import sys
 
 setuptools.setup(
     name = "supermann",
-    version = '1.3.1',
+    version = '1.3.2',
 
     author = "Sam Clements",
     author_email = "sam.clements@datasift.com",

supermann/__init__.py
@@ -4,6 +4,6 @@ from __future__ import absolute_import, unicode_literals
 
 from supermann.core import Supermann
 
-__version__ = '1.3.0'
+__version__ = '1.3.2'
 __author__ = 'Sam Clements <sam.clements@datasift.com>'
 __all__ = ['Supermann']

supermann/core.py
@@ -6,7 +6,8 @@ import collections
 import os
 
 import psutil
-import riemann
+import riemann.client
+import riemann.transport
 
 import supermann.metrics
 import supermann.signals
@@ -24,7 +25,7 @@ class Supermann(object):
         # The Supervisor and Riemann clients read their configuration from
         # the environment, though the Riemann client can be overridden
         self.supervisor = supermann.supervisor.Supervisor()
-        self.riemann_transport = riemann.client.TCPTransport(host, port)
+        self.riemann_transport = riemann.transport.TCPTransport(host, port)
         self.riemann = riemann.client.QueuedClient(self.riemann_transport)
 
     def connect(self, signal, reciver):
@@ -33,21 +34,18 @@ class Supermann(object):
 
     def run(self):
         """Runs forever, ensuring Riemann is disconnected properly"""
-        try:
-            self.riemann.connect()
-            for event in self.supervisor.run_forever():
-                # Emit a signal for each event
-                supermann.signals.event.send(self, event=event)
-                # Emit a signal for each Supervisor subprocess
-                self.emit_processes(event=event)
-                # Send the queued events at the end of the cycle
-                self.riemann.flush()
-        except Exception as exception:
-            self.log.exception("A fatal exception has occurred:")
-            raise exception
-        finally:
-            # Ensure the Riemann client is closed if we crash
-            self.riemann.disconnect()
+        with self.riemann:
+            try:
+                for event in self.supervisor.run_forever():
+                    # Emit a signal for each event
+                    supermann.signals.event.send(self, event=event)
+                    # Emit a signal for each Supervisor subprocess
+                    self.emit_processes(event=event)
+                    # Send the queued events at the end of the cycle
+                    self.riemann.flush()
+            except Exception as exception:
+                self.log.exception("A fatal exception has occurred:")
+                raise exception
 
     def emit_processes(self, event):
         """Emit a signal for each Supervisor child process"""