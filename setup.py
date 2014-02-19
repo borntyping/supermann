876d44251448ed173080935b1a89efef46b28a86
Sam Clements <sam.clements@datasift.com>
2014-02-19 16:19:22 +0000

    Release version 1.8.0
    - Set command line defaults correctly
    - Added process:%:uptime metric
    - Improved exception handling
    - Log basic information about Riemann on startup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 
 setuptools.setup(
     name = "supermann",
-    version = '1.8.0-dev',
+    version = '1.8.0',
 
     author = "Sam Clements",
     author_email = "sam.clements@datasift.com",
