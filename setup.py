66cf3a9570b9114eedc5da34b48e94b11ffeddd5
Sam Clements <sam@borntyping.co.uk>
2016-06-29 13:10:13 +0100

    Bump version to 3.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name = "supermann",
-    version = '3.1.0',
+    version = '3.2.0',
 
     author = "Sam Clements",
     author_email = "sam.clements@datasift.com",
--- supermann/__init__.py
+++ supermann/__init__.py
@@ -4,6 +4,6 @@ from __future__ import absolute_import
 
 from supermann.core import Supermann
 
-__version__ = '3.1.0'
+__version__ = '3.2.0'
 __author__ = 'Sam Clements <sam.clements@datasift.com>'
 __all__ = ['Supermann']
